d90b2f8a822966d7f5fe18804925145c22d6630c
gryc3k <gryc3k@gmail.com>
2026-03-26 09:03:39 +0100

    Konfiguracja klasy startowej Spring Boot i poprawka pom.xml

---
+++ src/main/java/com/mycompany/projektroboty/KosiarkobotApplication.java
@@ -0,0 +1,11 @@
+package com.mycompany.projektroboty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KosiarkobotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KosiarkobotApplication.class, args);
+    }
+}
